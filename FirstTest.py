a258c1b9c004722485e7e4a7a048d5eeade0bd19
vinayksk <vinayksk@gmail.com>
2016-06-25 21:36:15 -0700

    uploaded the first program that used the python yelp api

---
+++ FirstTest.py
@@ -0,0 +1,19 @@
+from yelp.client import Client
+from yelp.oauth1_authenticator import Oauth1Authenticator
+
+auth = Oauth1Authenticator(
+    consumer_key = "4mKzLgbw5zlE-O7Jle9kJg",
+    consumer_secret = "xQVDkzJQmbr26heYvjmRT6vCyuY",
+    token = "jStDNSrNExPPOYbafiRrnL01AphMONJH",
+    token_secret = "EH3SLPVzI4qKGxPOtlYRyPgcpkI"
+)
+
+client = Client(auth)
+
+params = {
+    'term': 'food',
+    'lang': 'en'
+}
+
+response = client.search('San Francisco', **params)
+print (response.businesses[0].name)
